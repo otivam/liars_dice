0f0f35b78d207e4ccccc7da407a851f52881001f
Gincho <otivam@gmail.com>
2022-07-03 15:57:12 +0300

    done - collection the data, some validations, left - play game logic

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ def players_generator(total_players,computer_players):
         if x < int(total_players) - int(computer_players):
             print("Hello player {}, what is your name ?".format(x+1))
             name = input()
-            my_data['player'+str(x+1)] = Player(name,dices_generator(),total_players)
+            my_data['player'+str(x+1)] = Player(name,dices_generator(),total_players,True,True)
         #generating computer players
         else:
             name = get_random_computer_name()
             print("Hello player {}, your name is {}.".format((x+1),name))
-            my_data['player'+str(x+1)] = Player(name,dices_generator(),total_players)
+            my_data['player'+str(x+1)] = Player(name,dices_generator(),total_players,False,True)
 
 
 def get_random_computer_name():
@@ -34,19 +34,19 @@ def get_random_computer_name():
     return name
 
 
-
 class Player:
-    def __init__(self,name,dices,total_players):
+    def __init__(self,name,dices,total_players,isHuman,isActive):
         self.name = name
         self.dices = dices
         self.total_players = total_players
+        self.isHuman = isHuman
+        self.isActive = isActive
 
     def play(self):
         result = []
         print("{}, it's your time to bid:".format(self.name))
         print("Please choose a dice number from 1 to 6")
-        bad_input_number = True
-        while bad_input_number:
+        while True:
             bid_number = input()
             if not bid_number.isdigit():
                 print("Please enter a digit")
@@ -55,12 +55,11 @@ class Player:
                     print("Please choose a dice number from 1 to 6 asdas")
                 else:
                     result.append(bid_number)
-                    bad_input_number = False
+                    break
 
         max_quantity_range = self.total_players*5
         print("How many '{}'s:".format(bid_number))
-        bad_input_quantity = True
-        while bad_input_quantity:
+        while True:
             bid_quantity = input()
             if not bid_quantity.isdigit():
                 print("Please enter a digit")
@@ -69,7 +68,7 @@ class Player:
                     print("Please choose a dice number from 1 to {}".format(max_quantity_range))
                 else:
                     result.append(bid_quantity)
-                    bad_input_quantity = False
+                    break
 
         return result
 
@@ -86,11 +85,56 @@ class Player:
 ######################
 print("Welcome to Liars dice, let's begin...")
 print("Please enter the total players:")
-total_players = input()
-print("And how many of them will be computers?")
-computer_players = input()
+while True:
+    total_players = input()
+    if not total_players.isdigit():
+        print("Please enter a digit")
+    elif total_players.isdigit():
+        if int(total_players) < 2:
+            print("The game is played by two or more players.")
+        else:
+            break
+
+
+print("And how many of them will be computer players?")
+while True:
+    computer_players = input()
+    if not computer_players.isdigit():
+        print("Please enter a digit")
+    elif total_players.isdigit():
+        if int(total_players) < int(computer_players):
+            print("Computer players cannot be more than total players!")
+        else:
+            break
+
 print("Very well! We will asign {} random computer players!".format(computer_players))
 players_generator(int(total_players),int(computer_players))
+
+
+
+while True:
+    #Checking for a winner
+    active_players = []
+    for x in range(total_players):
+        if my_data['player'+str(x+1)].isActive:
+            active_players.append(my_data['player'+str(x+1)])
+    #WINNER!
+    if len(active_players) == 1:
+        print("Great job, {}! YOU WIN!".format(active_players[0].name))
+        print("You have {} dices left.".format(active_players[0].dices))
+        print("Your reward is 1 banana.")
+        break
+    #No winner
+    else:
+        #PLAY THE GAME
+
+
+    
+
+
+
+
+
 
 
 '''
